a4a8ac2248080a838d97276ecc76eff3f273781a
DavidCarroll <dalcarro@ucsc.edu>
2020-07-06 18:25:21 -0700

    Halfway done

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,35 +3,51 @@ class Play extends Phaser.Scene{
         super("playScene");
     }
 //modifying prexisting phaser stuff. needs to be exact name and order.
-    // init(){
-
-    // }
+    // init(){}
     preload(){
-        
+        this.music = this.sound.add('demSpaceMuse','./assets/demented_space_music.wav')
+        this.backSwitch = false;
         //use PNG over jpg
-        this.load.image("rocket", "./assets/rocket.png");
+        this.load.image("rocket", "./assets/rocket2.png");
         this.load.image("spaceship", "./assets/spaceship.png");
-        this.load.image("starfield", "./assets/starfield.png");
+        this.load.image("altSpace", "./assets/altSpace3.png");
+        this.load.image("left_and_right", "./assets/lAr.png");
+        this.load.image("botAtop", "./assets/botAtop.png");
         //load spritesheet
         this.load.spritesheet('explosion','./assets/explosion.png',{frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
 
     create(){
+        let musConig = {
+            mute: false,
+            volume: 1,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: true,
+            delay: 0
+        };
+        this.music.play(musConig);
+        this.explodeList = ['altFire','altFire2','altFire3','altFire4'];
         //place tile sprite big
-        this.starfield = this.add.tileSprite(0, 0, 640,480, 'starfield').setOrigin(0,0)
-        //white rectangle boarders
-        this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(5, 5, 32, 455, 0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0,0);
-        //green UI background
-        this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0,0);
-        //add rocket(p1)
-        this.p1Rocket = new Rocket(this, game.config.width/2,431,'rocket',0).setScale(0.5,0.5).setOrigin(0,0);
-        //
+        this.starfield = this.add.tileSprite(0, 0, 640,480, 'altSpace').setOrigin(0,0);
+        //ship declarations
         this.ship01 = new Spaceship(this, game.config.width + 192, 132, 'spaceship',0,30).setOrigin(0,0);
         this.ship02 = new Spaceship(this, game.config.width + 96, 196, 'spaceship',0,30).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, 260, 'spaceship',0,30).setOrigin(0,0);
+        //white rectangle Rectangle(x, y, width, height);
+            //top
+        this.add.tileSprite(0, 0, 640, 32, "botAtop").setOrigin(0,0);
+            //bottem
+        this.add.tileSprite(0, 455, 640, 32, "botAtop").setOrigin(0,0);
+            //left
+        this.add.tileSprite(0, 32, 32, 455, "left_and_right").setOrigin(0,0);
+            //right
+        this.add.tileSprite(608, 0, 32, 457, "left_and_right").setOrigin(0,0);
+        //green UI background
+        this.add.tileSprite(37, 42, 566, 64, 0x00FF00).setOrigin(0,0);
+        //add rocket(p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2,431,'rocket',0).setScale(0.5,0.5).setOrigin(0,0);
         //define Keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -43,7 +59,8 @@ class Play extends Phaser.Scene{
             frameRate: 30
         });
         //score
-        this.p1Score =0;
+        this.p1Score = 0;
+        
         //score display
         let scoreConfig = {
             fontFamily: 'Courier',
@@ -58,6 +75,9 @@ class Play extends Phaser.Scene{
             fixedWidth: 100
         }
         this.scoreLeft = this.add.text(69,54, this.p1Score, scoreConfig);
+        
+        this.tempTime = game.settings.gameTimer/1000;
+        
         //game over flag
         this.gameOver = false;
         //60-second play clock
@@ -69,8 +89,26 @@ class Play extends Phaser.Scene{
         scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         },null,this);
+        this.timerUi = this.add.text(455,54, this.tempTime - this.clock.getElapsedSeconds(), scoreConfig);
     }
     update(){
+        this.timerUi.text = this.tempTime - Math.round(this.clock.getElapsedSeconds());
+        // if(this.gameOver && this.scoreLeft >= score){
+        //     score = this.scoreLeft
+        //    this.highScore = this.add.text(138,54, score, scoreConfig);
+        // }
+        if(this.clock == game.settings.gameTimer/2){
+            game.settings.spaceshipSpeed *= 2;
+            console.log("Speed increased");
+        }
+        if(this.backSwitch == false && !this.gameOver){
+            this.music.resume();
+            this.backSwitch = true;
+        }
+        if(this.gameOver == true){
+            this.music.pause();
+            this.backSwitch = false;
+        }
         if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyF)){
             this.scene.restart(this.p1Score);
         }
@@ -84,27 +122,18 @@ class Play extends Phaser.Scene{
             this.ship02.update();
             this.ship03.update();
         }
-
         //check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)){
-            console.log('kaboom ship 03');
             this.p1Rocket.reset();
-            this.shipExplode(this.ship03);
-            
-            
+            this.shipExplode(this.ship03); 
         }
         if(this.checkCollision(this.p1Rocket, this.ship02)){
-            console.log('kaboom ship 02');
             this.p1Rocket.reset();
             this.shipExplode(this.ship02);
-            
-            
         }
         if(this.checkCollision(this.p1Rocket, this.ship01)){
-            console.log('kaboom ship 01');
             this.p1Rocket.reset();
             this.shipExplode(this.ship01);
-            
         }
         if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)){
             this.scene.start('menuScene');
@@ -134,7 +163,8 @@ class Play extends Phaser.Scene{
         //score increment and repaint
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
-        this.sound.play('sfx_explosion');
+        //code below from https://www.w3schools.com/jsref/jsref_random.asp
+        this.sound.play(this.explodeList[Math.floor(Math.random() * 4)]);
     }
     
 }
